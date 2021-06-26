c079b743a600e5c531fc80a18c2a1fb37d43f46b
mkmardel <mkmardel@gmail.com>
2021-06-26 15:01:00 +0200

    privacy banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Blogtoday';
+
+  ngOnInit(): void {
+    this.initPrivacyBar();
+  }
+
+  initPrivacyBar() {
+    setTimeout(() => {
+      if (!localStorage.bannerClosed) {
+        $('.privacy-banner').css('display', 'inherit');
+      } else {
+        $('.privacy-banner').css('display', 'none');
+      }
+      $('.privacy-banner button').click(function () {
+        $('.privacy-banner').css('display', 'none');
+        localStorage.bannerClosed = 'true';
+      });
+      $('.banner-accept').click(function () {
+        $('.privacy-banner').css('display', 'none');
+        localStorage.bannerClosed = 'true';
+      });
+      if (navigator.userAgent.match(/Opera|OPR\//)) {
+        $('.privacy-banner').css('display', 'inherit');
+      }
+    }, 2000);
+  }
 }
